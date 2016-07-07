683c3c2ba93a10053248d175dd11e80914602923
Daniel Adornes <daniel.adornes@gmail.com>
2016-07-07 15:01:37 -0300

    Shortening code

---
+++ collatz_ruby.rb
@@ -0,0 +1,25 @@
+#
+# This version in Ruby exploits some nice features of the language and
+# the functional programming paradigm, such as recursivity, ...
+#
+object CollatzConjecture extends App {
+  case class Experiment(val number: Int, val steps: List[Int])
+
+  def collatz_steps(i: Int): List[Int] = {
+    require(i > 0, "the number must be greater than zero.")
+  
+	  def iterate(i: Int, l: List[Int]): List[Int] =
+	    if(i == 1) i :: l
+	    else if(i % 2 == 0) iterate(i/2, i :: l)
+	    else iterate(i * 3  + 1, i :: l)
+	  
+	  iterate(i, List.empty).reverse
+  }
+  
+  def from(n: Int): Stream[Int] = n #:: from(n + 1)
+  
+  override def main( args:Array[String] ):Unit = {
+    val experiments = from(1).map(i => Experiment(i, collatz_steps(i)))    
+    experiments.take(100).toList.foreach(println)  
+  }
+}
--- collatz_scala.scala
+++ collatz_scala.scala
@@ -3,24 +3,22 @@
  * the functional programming paradigm, such as recursivity, Streams and
  * case classes.
  */
-object CollatzConjecture extends App {
-  case class Experiment(val number: Int, val steps: List[Int])
 
-  def collatz_steps(i: Int): List[Int] = {
-    require(i > 0, "the number must be greater than zero.")
+case class Experiment(val number: Int, val steps: List[Int])
+
+def collatz_steps(i: Int): List[Int] = {
+  require(i > 0, "the number must be greater than zero.")
+
+  def iterate(i: Int, l: List[Int]): List[Int] =
+    if(i == 1) i :: l
+    else if(i % 2 == 0) iterate(i/2, i :: l)
+    else iterate(i * 3  + 1, i :: l)
   
-	  def iterate(i: Int, l: List[Int]): List[Int] =
-	    if(i == 1) i :: l
-	    else if(i % 2 == 0) iterate(i/2, i :: l)
-	    else iterate(i * 3  + 1, i :: l)
-	  
-	  iterate(i, List.empty).reverse
-  }
-  
-  def from(n: Int): Stream[Int] = n #:: from(n + 1)
-  
-  override def main( args:Array[String] ):Unit = {
-    val experiments = from(1).map(i => Experiment(i, collatz_steps(i)))    
-    experiments.take(100).toList.foreach(println)  
-  }
+  iterate(i, List.empty).reverse
 }
+
+def from(n: Int): Stream[Int] = n #:: from(n + 1)
+
+val experiments = from(1).map(i => Experiment(i, collatz_steps(i)))
+
+experiments.take(100).toList.foreach(println)
